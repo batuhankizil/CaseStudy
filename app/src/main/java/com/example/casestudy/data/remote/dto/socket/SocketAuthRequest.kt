3bb8f631507ec93afb88b4773f6013800cc3cbb3
Batuhan <batuhan.kizil@outlook.com>
2025-12-11 02:40:15 +0300

    Add socket auth request and response models

---
+++ app/src/main/java/com/example/casestudy/data/remote/dto/socket/SocketAuthRequest.kt
@@ -0,0 +1,6 @@
+package com.example.casestudy.data.remote.dto.socket
+
+data class SocketAuthRequest(
+    val socket_id: String,
+    val channel_name: String
+)
---
+++ app/src/main/java/com/example/casestudy/data/remote/dto/socket/SocketAuthResponse.kt
@@ -0,0 +1,5 @@
+package com.example.casestudy.data.remote.dto.socket
+
+data class SocketAuthResponse(
+    val auth: String
+)
