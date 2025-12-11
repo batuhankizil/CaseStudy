c2461e760bd51ab868f21a3f684b54144ff8aa35
Batuhan <batuhan.kizil@outlook.com>
2025-12-11 21:53:31 +0300

    Add AppNavGraph and Screen definitions

---
+++ app/src/main/java/com/example/casestudy/ui/navigation/AppNavGraph.kt
@@ -0,0 +1,42 @@
+package com.example.casestudy.ui.navigation
+
+import CreateRestaurantScreen
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.casestudy.ui.auth.LoginScreen
+import com.example.casestudy.ui.auth.RegisterScreen
+import com.example.casestudy.ui.map.MapScreen
+import com.example.casestudy.ui.order.OrderListScreen
+import com.example.casestudy.ui.restaurant.RestaurantDetailScreen
+
+@Composable
+fun AppNavGraph(
+    navController: NavHostController,
+    startDestination: String = Screen.Login.route
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable(Screen.Login.route) {
+            LoginScreen(navController = navController)
+        }
+        composable(Screen.Register.route) {
+            RegisterScreen(navController = navController)
+        }
+        composable(Screen.OrderList.route) {
+            OrderListScreen(navController = navController)
+        }
+        composable(Screen.CreateRestaurant.route) {
+            CreateRestaurantScreen(navController = navController)
+        }
+        composable(Screen.RestaurantDetail.route) {
+            RestaurantDetailScreen(navController = navController)
+        }
+        composable(Screen.Map.route) {
+            MapScreen(navController = navController)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/casestudy/ui/navigation/Screen.kt
@@ -0,0 +1,10 @@
+package com.example.casestudy.ui.navigation
+
+sealed class Screen(val route: String) {
+    object Login : Screen("login")
+    object Register : Screen("register")
+    object CreateRestaurant : Screen("create_restaurant")
+    object RestaurantDetail : Screen("restaurant_detail")
+    object OrderList : Screen("order_list")
+    object Map : Screen("map")
+}
