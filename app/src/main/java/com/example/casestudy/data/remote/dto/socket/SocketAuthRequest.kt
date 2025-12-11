407ce3b6696efa5eb31dc42bc5e525a7167f2a25
Batuhan <batuhan.kizil@outlook.com>
2025-12-11 20:53:05 +0300

    Add socket auth request and response models

--- app/src/main/java/com/example/casestudy/data/remote/dto/socket/SocketAuthRequest.kt
+++ app/src/main/java/com/example/casestudy/data/remote/dto/socket/SocketAuthRequest.kt
@@ -1,6 +1,10 @@
 package com.example.casestudy.data.remote.dto.socket
 
+import com.google.gson.annotations.SerializedName
+
 data class SocketAuthRequest(
+    @SerializedName("socket_id")
     val socket_id: String,
+    @SerializedName("channel_name")
     val channel_name: String
 )
--- app/src/main/java/com/example/casestudy/data/remote/dto/socket/SocketAuthResponse.kt
+++ app/src/main/java/com/example/casestudy/data/remote/dto/socket/SocketAuthResponse.kt
@@ -1,5 +1,8 @@
 package com.example.casestudy.data.remote.dto.socket
 
+import com.google.gson.annotations.SerializedName
+
 data class SocketAuthResponse(
+    @SerializedName("auth")
     val auth: String
 )
