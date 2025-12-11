2c18941b2647d20322950943bfcfe42c9b7f2bea
Batuhan <batuhan.kizil@outlook.com>
2025-12-11 18:10:39 +0300

    Add user model and DTO

---
+++ app/src/main/java/com/example/casestudy/data/remote/dto/auth/UserDto.kt
@@ -0,0 +1,12 @@
+package com.example.casestudy.data.remote.dto.auth
+
+import com.google.gson.annotations.SerializedName
+
+data class UserDto(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("email")
+    val email: String,
+)
