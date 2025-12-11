6e2c4b0ebeff1c08fdde27e063b14af91b15daa4
Batuhan <batuhan.kizil@outlook.com>
2025-12-11 23:35:09 +0300

    Add network module with dependency injection for Retrofit and OkHttp

---
+++ app/src/main/java/com/example/casestudy/data/di/NetworkModule.kt
@@ -0,0 +1,79 @@
+package com.example.casestudy.data.di
+
+import java.util.concurrent.TimeUnit
+import com.example.casestudy.data.local.preferences.AuthPreferences
+import com.example.casestudy.data.remote.api.ApiService
+import com.example.casestudy.data.remote.network.AuthInterceptor
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+import kotlin.jvm.java
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val BASE_URL = "http://188.34.155.223/new-qr-menu/api/"
+
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthInterceptor(
+        prefs: AuthPreferences
+    ): AuthInterceptor = AuthInterceptor(prefs)
+
+    @Provides
+    @Singleton
+    fun providesOkHttpClient(
+        logging: HttpLoggingInterceptor,
+        authInterceptor: AuthInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(authInterceptor)
+            .addInterceptor(logging)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .retryOnConnectionFailure(true)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        client: OkHttpClient
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(
+        retrofit: Retrofit
+    ): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson = Gson()
+
+}
