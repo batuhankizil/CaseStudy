293d03b2ebf240ff4f555a700ce365493f320dda
Batuhan <batuhan.kizil@outlook.com>
2025-12-11 20:55:52 +0300

    Add DataStoreModule for providing Preferences DataStore

---
+++ app/src/main/java/com/example/casestudy/data/di/DataStoreModule.kt
@@ -0,0 +1,28 @@
+package com.example.casestudy.data.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+
+    @Provides
+    @Singleton
+    fun providePreferencesDataStore(
+        @ApplicationContext context: Context
+    ): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(
+            produceFile = { context.preferencesDataStoreFile("app_prefs") }
+        )
+    }
+}
